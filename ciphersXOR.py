ab69ce69eff693e22e15fa36562f2b5dea33b39a
LukeSev <lloughner00@gmail.com>
2023-01-20 11:22:56 -0800

    Done with Task2B, onto C

--- ciphersXOR.py
+++ ciphersXOR.py
@@ -4,13 +4,13 @@ def match_key(plaintext, key):
     # Matches lengths of plaintext/key by either making key larger or making key smaller
     # Returns key
     plainArr = bytearray(plaintext)
-    newKey = bytearray(key)
-    if(len(plaintext) > len(key)):
+    newKey = bytearray([key])
+    if(len(plainArr) > len(newKey)):
         i = 0
         while(len(plainArr) > len(newKey)):
             newKey.append(newKey[i])
             i += 1
-    elif(len(plaintext) < len(key)):
+    elif(len(plainArr) < len(newKey)):
         newKey = newKey[:len(plainArr)]
     return bytes(newKey)
 
@@ -19,24 +19,46 @@ def xor_bytestrings(plaintext, key):
     return bytes([x ^ y for x, y in zip(plaintext, newKey)])
 
 def main():
-    # Basic functionality test
-    test_b1 = b'10110011'
-    test_b2 = b'01001100'
-    out1 = xor_bytestrings(test_b1, test_b2) # Should return all 0s
-    out2 = xor_bytestrings(test_b1, test_b1) # Should return all 1s
 
-    print(out1)
-    print(out2)
+    #test = "basic"
+    test = "complex"
 
-    # Test key length matching
-    test_b3 = b'00000000'
-    test_b4 = b'111'
-    test_b5 = b'00'
-    out3 = xor_bytestrings(test_b3, test_b4) # Should return all 1s
-    out4 = xor_bytestrings(test_b3, test_b5) # Should return all 0s
+    if(test == "basic"):
+        # Basic functionality test
+        test_b1 = b'10110011'
+        test_b2 = b'01001100'
+        out1 = xor_bytestrings(test_b1, test_b2) # Should return all 0s
+        out2 = xor_bytestrings(test_b1, test_b1) # Should return all 1s
 
-    print(out3)
-    print(out4)
+        #print(out1)
+        #print(out2)
+
+        # Test key length matching
+        test_b3 = bytes.fromhex("ff")
+        test_b4 = bytes.fromhex("00")
+        test_b5 = bytes.fromhex("ff")
+        out3 = xor_bytestrings(test_b3, test_b4) # Should return all 1s
+        out4 = xor_bytestrings(test_b3, test_b5) # Should return all 0s
+
+        print(out3)
+        print(out4)
+
+    elif(test == "complex"):
+        # We want to take a word, xor it and encode as hex string, then try to decode in reverse
+        key = 16 # Single byte key
+        plaintext = "Hello, this is a test"
+        print("Plaintext before encryption: " + plaintext)
+
+        # Now XOR
+        heXORed = xor_bytestrings(plaintext.encode("ascii"), bytes(key)).hex()
+        print("XORed and hexxed: " + heXORed)
+
+        # Now convert back again
+        ciphertext = bytes.fromhex(heXORed)
+        plaintext_surely = xor_bytestrings(ciphertext, bytes(key)).decode("ascii")
+        print("Decrypted: " + plaintext_surely)
+
+
 
 if __name__ == '__main__':
     main()
